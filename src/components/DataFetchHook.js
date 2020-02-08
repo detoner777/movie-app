6a36ccc32349deaa8344f90d48d83644ea220366
Alexey_Lisenko <detoner777@gmail.com>
2020-02-08 21:40:41 +0200

    fetch Hook

--- src/components/DataFetchHook.js
+++ src/components/DataFetchHook.js
@@ -2,3 +2,42 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const API_KEY = process.env.TMDB_API_KEY;
+
+const useAxiosHook = myUrl => {
+  const [data, setData] = useState([]);
+  const [url, setUrl] = useState(myUrl);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isErrorm, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const response = await axios.get(url);
+
+        await Promise.all(
+          response.data.results.map(async movie => {
+            const responseDetails = await axios.get(
+              `https://api.themoviedb.org/3/movie/${movie.id}?api_key=${API_KEY}&&language=en-US&append_to_response=videos,images&include_image_language=en,null`
+            );
+            movie.details = responseDetails.data;
+          })
+        );
+
+        setData(response.data.results);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false); // Should trigger loading spinner, or throw error if axios request fails.
+    };
+
+    fetchData();
+  }, [url]);
+
+  return [{ data, isLoading, isError }, setUrl];
+};
+
+export default useAxiosHook;
